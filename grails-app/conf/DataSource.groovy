7da614e87ea69c49241eb4c817c6b26c73b30f66
jay-chen <jack08300@hotmail.com>
2013-08-08 20:39:24 -0400

    init

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,12 +1,12 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "my"
+    password = "W8RRSSS2EI09 "
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
+    cache.use_query_cache = true
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 // environment specific settings
@@ -14,19 +14,25 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://my-outfit.com/my_outfit?useUnicode=yes&characterEncloding=UTF-8"
+            username = "my"
+            password = "W8RRSSS2EI09 "
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://my-outfit.com/my_outfit?useUnicode=yes&characterEncloding=UTF-8"
+            username = "my"
+            password = "W8RRSSS2EI09 "
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://my-outfit.com/my_outfit?useUnicode=yes&characterEncloding=UTF-8"
+            username = "my"
+            password = "W8RRSSS2EI09 "
             pooled = true
             properties {
                maxActive = -1
---
+++ grails-app/domain/helpbuy/User.groovy
@@ -0,0 +1,7 @@
+package helpbuy
+
+class User {
+
+    static constraints = {
+    }
+}
